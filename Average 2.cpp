88e5a4b2e5ee38a849200d0b128c89cf1401984a
Mo-hamado <90215830+Mo-hamado@users.noreply.github.com>
2021-11-13 13:18:53 +0200

    Add files via upload

---
+++ Average 2.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+
+	double a, b, c, media;
+	cin >> a >> b >> c;
+	a *= 2;
+	b *= 3;
+	c *= 5;
+	media = (a + b + c) / (2 + 3 + 5);
+	cout << "MEDIA = " << fixed << setprecision(1) << media << endl;
+
+	return 0;
+}
