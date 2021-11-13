ddae54a4e1b636fe97677fcf790fcbd700e1d300
Mo-hamado <90215830+Mo-hamado@users.noreply.github.com>
2021-11-13 13:25:08 +0200

    Add files via upload

---
+++ Simple Calculate.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+
+	int code_p1, num_unit_p1;
+	double price_unit_p1;
+	int code_p2, num_unit_p2;
+	double price_unit_p2;
+	cin >> code_p1 >> num_unit_p1 >> price_unit_p1 >> code_p2 >> num_unit_p2 >> price_unit_p2;
+	cout << "VALOR A PAGAR: R$ " << fixed << setprecision(2) << (num_unit_p1 * price_unit_p1) + (num_unit_p2 * price_unit_p2) << endl;
+
+	return 0;
+}
