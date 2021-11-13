84ef550175b2ad4f6bdd01b988a3a0c1e855a8cc
Mo-hamado <90215830+Mo-hamado@users.noreply.github.com>
2021-11-13 13:37:36 +0200

    Add files via upload

---
+++ 1017.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+
+	int t, v;
+	float d;
+	cin >> t >> v;
+	d = v * t;
+	cout << fixed << setprecision(3) << (d / 12) << endl;
+
+	return 0;
+}
---
+++ 1018.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int banknt, r100, r50, r20, r10, r5, r2, r1;
+    cin >> banknt;
+    r100 = banknt / 100;
+    r50 = (banknt % 100) / 50;
+    r20 = (banknt % 100 % 50) / 20;
+    r10 = (banknt % 100 % 50 % 20) / 10;
+    r5 = (banknt % 100 % 50 % 20 % 10) / 5;
+    r2 = (banknt % 100 % 50 % 20 % 10 % 5) / 2;
+    r1 = banknt % 100 % 50 % 20 % 10 % 5 % 2;
+    cout << banknt << "\n" << r100 << " nota(s) de R$ 100,00\n" << r50 << " nota(s) de R$ 50,00\n" << r20 << " nota(s) de R$ 20,00\n" << r10 << " nota(s) de R$ 10,00\n" << r5 << " nota(s) de R$ 5,00\n" << r2 << " nota(s) de R$ 2,00\n" << r1 << " nota(s) de R$ 1,00\n";
+
+    return 0;
+}
